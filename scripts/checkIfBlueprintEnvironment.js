46ad35e6177290c03fb6fe09c928cd3d582fe1d4
VillageR88 <villager88pl@gmail.com>
2024-11-10 21:59:24 +0100

    Fixed checkIfBlueprintEnvironment
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- scripts/checkIfBlueprintEnvironment.js
+++ scripts/checkIfBlueprintEnvironment.js
@@ -5,6 +5,7 @@ export default function checkIfBlueprintEnvironment() {
 		global.id.mainBlueprintStyleAdd.style.display === "flex" ||
 		global.id.mainBlueprintStateSelector.style.display === "flex" ||
 		global.id.mainBlueprintStateAdd.style.display === "flex" ||
-		global.id.mainBlueprintStateStyleSelector.style.display === "flex";
+		global.id.mainBlueprintStateStyleSelector.style.display === "flex" ||
+		global.id.mainBlueprintAttributeSelector.style.display === "flex";
 	return isBlueprint;
 }
