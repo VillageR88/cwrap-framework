fc13c1443e8453b52d56890c10999d57f8950a58
VillageR88 <villager88pl@gmail.com>
2024-11-27 18:59:23 +0100

    Improved applyStyles.js
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- scripts/applyStyles.js
+++ scripts/applyStyles.js
@@ -5,95 +5,125 @@
  * Applies the styles from the cssMap and mediaQueriesMap to preview document (iframe).
  */
 export default function applyStyles() {
-	const rootMap = global.map.rootMap;
-	const fontMap = global.map.fontMap;
-	const classroomMap = global.map.classroomMap;
-	const cssMap = global.map.cssMap;
-	const mediaQueriesMap = global.map.mediaQueriesMap;
+  const rootMap = global.map.rootMap;
+  const fontMap = global.map.fontMap;
+  const classroomMap = global.map.classroomMap;
+  const cssMap = global.map.cssMap;
+  const mediaQueriesMap = global.map.mediaQueriesMap;
 
-	let customStyles = "";
-	const mediaQueries = {};
+  let customStyles = "";
+  const mediaQueries = {};
 
-	if (classroomMap) {
-		classroomMap.forEach((value, key) => {
-			let hashtag = "";
-			if (value.type === "class") {
-				hashtag = ".";
-			} else if (value.type === "id") {
-				hashtag = "#";
-			} else if (value.type === "pseudo:") {
-				hashtag = ":";
-			} else if (value.type === "pseudo::") {
-				hashtag = "::";
-			}
+  if (classroomMap) {
+    classroomMap.forEach((value, key) => {
+      let hashtag = "";
+      if (value.type === "class") {
+        hashtag = ".";
+      } else if (value.type === "id") {
+        hashtag = "#";
+      } else if (value.type === "pseudo:") {
+        hashtag = ":";
+      } else if (value.type === "pseudo::") {
+        hashtag = "::";
+      }
 
-			if (value.style) {
-				customStyles += `${hashtag}${value.name} {${value.style}}\n`;
-			}
+      if (value.style) {
+        customStyles += `${hashtag}${value.name} {${value.style}}\n`;
+      }
 
-			if (value.mediaQueries) {
-				for (const mediaQuery of value.mediaQueries) {
-					if (!mediaQueries[mediaQuery.query]) {
-						mediaQueries[mediaQuery.query] = "";
-					}
-					mediaQueries[mediaQuery.query] +=
-						`${hashtag}${value.name} {${mediaQuery.style}}\n`;
-				}
-			}
-		});
-	}
+      if (value.mediaQueries) {
+        for (const mediaQuery of value.mediaQueries) {
+          if (!mediaQueries[mediaQuery.query]) {
+            mediaQueries[mediaQuery.query] = "";
+          }
+          mediaQueries[
+            mediaQuery.query
+          ] += `${hashtag}${value.name} {${mediaQuery.style}}\n`;
+        }
+      }
+    });
+  }
 
-	if (fontMap) {
-		for (const [_, fonts] of fontMap.entries()) {
-			for (const font of fonts) {
-				customStyles += "@font-face {\n";
-				for (const [property, value] of Object.entries(font)) {
-					customStyles += `  ${property}: ${value};\n`;
-				}
-				customStyles += "}\n";
-			}
-		}
-	}
+  if (fontMap) {
+    for (const [_, fonts] of fontMap.entries()) {
+      for (const font of fonts) {
+        customStyles += "@font-face {\n";
+        for (const [property, value] of Object.entries(font)) {
+          customStyles += `  ${property}: ${value};\n`;
+        }
+        customStyles += "}\n";
+      }
+    }
+  }
 
-	if (rootMap) {
-		customStyles += ":root {\n";
-		rootMap.forEach((value, key) => {
-			customStyles += `${key}: ${value};\n`;
-		});
-		customStyles += "}\n";
-	}
+  if (rootMap) {
+    customStyles += ":root {\n";
+    rootMap.forEach((value, key) => {
+      customStyles += `${key}: ${value};\n`;
+    });
+    customStyles += "}\n";
+  }
 
-	//type of value is object or string
-	cssMap.forEach((value, key) => {
-		customStyles += `${key} {${value}}\n`;
-	});
+  //type of value is object or string
+  cssMap.forEach((value, key) => {
+    customStyles += `${key} {${value}}\n`;
+  });
 
-	// customStyles = addCustomClasses(customStyles);
+  // customStyles = addCustomClasses(customStyles);
 
-	if (mediaQueriesMap) {
-		mediaQueriesMap.forEach((styles, query) => {
-			if (!mediaQueries[query]) {
-				mediaQueries[query] = "";
-			}
-			styles.forEach((value, key) => {
-				mediaQueries[query] += `${key} {${value}}\n`;
-			});
-		});
-	}
+  if (mediaQueriesMap) {
+    mediaQueriesMap.forEach((styles, query) => {
+      if (!mediaQueries[query]) {
+        mediaQueries[query] = "";
+      }
+      styles.forEach((value, key) => {
+        mediaQueries[query] += `${key} {${value}}\n`;
+      });
+    });
+  }
 
-	// Append collected media queries at the end
-	for (const [query, styles] of Object.entries(mediaQueries)) {
-		customStyles += `@media (${query}) {\n${styles}}\n`;
-	}
+  const minWidthMediaQueries = {};
+  const sortedMinWidthQueries = Object.keys(mediaQueries)
+    .filter((query) => query.includes("min-width"))
+    .sort((a, b) => {
+      const minWidthA = Number.parseInt(
+        a.match(/min-width:\s*(\d+)/)?.[1] || 0,
+        10
+      );
+      const minWidthB = Number.parseInt(
+        b.match(/min-width:\s*(\d+)/)?.[1] || 0,
+        10
+      );
+      return minWidthA - minWidthB;
+    });
+  for (const query of sortedMinWidthQueries) {
+    minWidthMediaQueries[query] = mediaQueries[query];
+  }
+  const maxWidthMediaQueries = {};
+  const sortedQueries = Object.keys(mediaQueries)
+    .filter((query) => query.includes("max-width"))
+    .sort((a, b) => {
+      const maxWidthA = Number.parseInt(
+        a.match(/max-width:\s*(\d+)/)?.[1] || 0,
+        10
+      );
+      const maxWidthB = Number.parseInt(
+        b.match(/max-width:\s*(\d+)/)?.[1] || 0,
+        10
+      );
+      return maxWidthB - maxWidthA;
+    });
+  for (const query of sortedQueries) {
+    maxWidthMediaQueries[query] = mediaQueries[query];
+  }
 
-	global.id.doc.getElementById("custom-styles").textContent = customStyles;
-}
+  for (const [query, styles] of Object.entries(minWidthMediaQueries)) {
+    customStyles += `@media (${query}) {\n${styles}}\n`;
+  }
 
-function addCustomClasses(customStyles) {
-	let updatedStyles = customStyles;
-	updatedStyles += ".cwrap-glowing {\n";
-	updatedStyles += "outline: 2px solid red;\n";
-	updatedStyles += "outline-offset: -2px;\n";
-	updatedStyles += "}\n";
-	return updatedStyles;
+  for (const [query, styles] of Object.entries(maxWidthMediaQueries)) {
+    customStyles += `@media (${query}) {\n${styles}}\n`;
+  }
+
+  global.id.doc.getElementById("custom-styles").textContent = customStyles;
 }
