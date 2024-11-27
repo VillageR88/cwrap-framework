c1f9eba32391cd3de14db3758410e5a61d77da50
VillageR88 <villager88pl@gmail.com>
2024-11-27 19:10:47 +0100

    Improved updateElementInfo.js
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- scripts/updateElementInfo.js
+++ scripts/updateElementInfo.js
@@ -13,98 +13,107 @@ import validateParentElement from "./validateParentElement.js";
  * @param {Map} mediaQueriesMap - The map of media queries.
  */
 export default function updateElementInfo(fullPath, element) {
-	const cssMap = global.map.cssMap;
-	const mediaQueriesMap = global.map.mediaQueriesMap;
-	const nameHelper = global.id.nameHelper;
+  const cssMap = global.map.cssMap;
+  const mediaQueriesMap = global.map.mediaQueriesMap;
+  const nameHelper = global.id.nameHelper;
 
-	// Set the value of the select element to the full path
-	nameHelper.textContent = fullPath;
-	let style = global.variable.style;
-	let currentStyle;
-	if (global.id.navAdditionalScreen.classList.contains("screenDesktop")) {
-		currentStyle = cssMap.get(fullPath);
-	} else {
-		let selectedValue;
-		if (global.id.navAdditionalScreen.classList.contains("screenTablet"))
-			selectedValue = "max-width: 768px";
-		else selectedValue = "max-width: 640px";
-		const mediaQueries = mediaQueriesMap.get(selectedValue);
-		const mediaQuery = mediaQueries?.get(fullPath);
-		style = mediaQuery || "No media query style";
-		currentStyle = mediaQuery;
-	}
-	style = currentStyle;
+  // Set the value of the select element to the full path
+  nameHelper.textContent = fullPath;
+  let style = global.variable.style;
+  let currentStyle;
+  if (global.id.navAdditionalScreen.classList.contains("screenDesktop")) {
+    currentStyle = cssMap.get(fullPath);
+  } else {
+    let selectedValue;
+    if (global.id.navAdditionalScreen.classList.contains("screenTablet"))
+      selectedValue = "max-width: 768px";
+    else if (global.id.navAdditionalScreen.classList.contains("screenMobile"))
+      selectedValue = "max-width: 640px";
+    else if (global.id.navAdditionalScreen.classList.contains("screenCustom")) {
+      selectedValue = global.id.navScreenCustom.value;
+    }
+    const mediaQueries = mediaQueriesMap.get(selectedValue);
+    const mediaQuery = mediaQueries?.get(fullPath);
+    style = mediaQuery || "No media query style";
+    currentStyle = mediaQuery;
+  }
+  style = currentStyle;
 
-	//because sometimes you select something like something:hover and it will not be selected that is why we need to check if the element
-	if (element) {
-		// Define the keyframes for the combined glowing effect
+  //because sometimes you select something like something:hover and it will not be selected that is why we need to check if the element
+  if (element) {
+    // Define the keyframes for the combined glowing effect
 
-		const selectionColor = {
-			red: "rgba(255, 0, 0, 1)",
-			green: "rgba(0, 255, 0, 1)",
-			blue: "rgba(0, 0, 255, 1)",
-		};
-		const selected = global.localSettings.selectionColor;
+    const selectionColor = {
+      red: "rgba(255, 0, 0, 1)",
+      green: "rgba(0, 255, 0, 1)",
+      blue: "rgba(0, 0, 255, 1)",
+    };
+    const selected = global.localSettings.selectionColor;
 
-		const keyframes = [
-			{
-				boxShadow: `0 0 10px ${selectionColor[selected]} inset, 0 0 10px ${selectionColor[selected]}`,
-			},
-			{
-				boxShadow: `0 0 20px ${selectionColor[selected].replace("1", "0.5")} inset, 0 0 20px ${selectionColor[selected].replace("1", "0.5")}`,
-			},
-			{ boxShadow: `0 0 10px ${selectionColor[selected]} inset, 0 0 10px ${selectionColor[selected]}` },
-		];
+    const keyframes = [
+      {
+        boxShadow: `0 0 10px ${selectionColor[selected]} inset, 0 0 10px ${selectionColor[selected]}`,
+      },
+      {
+        boxShadow: `0 0 20px ${selectionColor[selected].replace(
+          "1",
+          "0.5"
+        )} inset, 0 0 20px ${selectionColor[selected].replace("1", "0.5")}`,
+      },
+      {
+        boxShadow: `0 0 10px ${selectionColor[selected]} inset, 0 0 10px ${selectionColor[selected]}`,
+      },
+    ];
 
-		// Define the timing options for the animation
-		const options = {
-			duration: 500, // Duration in milliseconds
-			iterations: 1, // Number of times the animation should repeat
-		};
+    // Define the timing options for the animation
+    const options = {
+      duration: 500, // Duration in milliseconds
+      iterations: 1, // Number of times the animation should repeat
+    };
 
-		// Apply the combined animation to the element
-		element.animate(keyframes, options);
-	}
+    // Apply the combined animation to the element
+    element.animate(keyframes, options);
+  }
 
-	// Split the current style string into individual properties, filter out empty strings, and trim whitespace
-	/**
-	 * @type {string[]} selectedValue
-	 */
-	let styleProperties;
-	if (currentStyle)
-		styleProperties = currentStyle
-			.split(";")
-			.filter(Boolean)
-			.map((prop) => prop.trim());
-	// Get the property select element by its ID
-	const propertySelect = global.id.propertySelect;
-	// Clear any existing options in the property select element
-	const propertySelectMemory = propertySelect.value;
-	propertySelect.innerHTML = "";
-	// Iterate over each style property using for...of loop
-	if (currentStyle)
-		for (const prop of styleProperties) {
-			// Split the property into key and value, and trim whitespace
-			const [key] = prop.split(":").map((item) => item.trim());
-			// Create a new option element
-			const option = document.createElement("option");
-			// Set the value and text content of the option element to the property key
-			option.value = key;
-			option.textContent = key;
-			// Append the option element to the property select element
-			propertySelect.appendChild(option);
-		}
-	if (
-		propertySelectMemory &&
-		propertySelect.querySelector(`option[value="${propertySelectMemory}"]`)
-	) {
-		propertySelect.value = propertySelectMemory;
-	}
+  // Split the current style string into individual properties, filter out empty strings, and trim whitespace
+  /**
+   * @type {string[]} selectedValue
+   */
+  let styleProperties;
+  if (currentStyle)
+    styleProperties = currentStyle
+      .split(";")
+      .filter(Boolean)
+      .map((prop) => prop.trim());
+  // Get the property select element by its ID
+  const propertySelect = global.id.propertySelect;
+  // Clear any existing options in the property select element
+  const propertySelectMemory = propertySelect.value;
+  propertySelect.innerHTML = "";
+  // Iterate over each style property using for...of loop
+  if (currentStyle)
+    for (const prop of styleProperties) {
+      // Split the property into key and value, and trim whitespace
+      const [key] = prop.split(":").map((item) => item.trim());
+      // Create a new option element
+      const option = document.createElement("option");
+      // Set the value and text content of the option element to the property key
+      option.value = key;
+      option.textContent = key;
+      // Append the option element to the property select element
+      propertySelect.appendChild(option);
+    }
+  if (
+    propertySelectMemory &&
+    propertySelect.querySelector(`option[value="${propertySelectMemory}"]`)
+  ) {
+    propertySelect.value = propertySelectMemory;
+  }
 
-	//populateAttributeOptions(previewDocument);
-	//populateAttributeOptionsValue();
-	validateRemoveElement();
-	validateParentElement();
-	global.id.elementSelect.value = nameHelper.textContent;
-	global.id.mainTemplatesSelectorParent.value = nameHelper.textContent;
+  //populateAttributeOptions(previewDocument);
+  //populateAttributeOptionsValue();
+  validateRemoveElement();
+  validateParentElement();
+  global.id.elementSelect.value = nameHelper.textContent;
+  global.id.mainTemplatesSelectorParent.value = nameHelper.textContent;
 }
