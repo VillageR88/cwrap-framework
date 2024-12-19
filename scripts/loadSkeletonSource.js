6843ab50797e0a52c4b5ef4ddb79f89041ec577f
VillageR88 <villager88pl@gmail.com>
2024-12-19 20:11:19 +0100

    Redone some server mistakes (commit on purpose)
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- scripts/loadSkeletonSource.js
+++ scripts/loadSkeletonSource.js
@@ -2,18 +2,20 @@ const currentPath = window.location.pathname;
 const skeletonApiUrl = `routes/${currentPath}/skeleton.json`;
 const skeletonEmptyTemplate = "templates/empty/routes/skeleton.json";
 
-
 export default async function loadSkeletonSource() {
-	const url = `${skeletonApiUrl}?v=${new Date().getTime()}`;
-	try {
-		const response = await fetch(url);
-		const jsonObj = await response.json();
-		return jsonObj;
-	} catch (error) {
-		console.warn("Skeleton source not found, loading empty template instead.");
-		const urlEmptyJsonTemplate = `${skeletonEmptyTemplate}?v=${new Date().getTime()}`;
-		const response = await fetch(urlEmptyJsonTemplate);
-		const jsonObj = await response.json();
-		return jsonObj;
-	}
+    const url = `${skeletonApiUrl}?v=${new Date().getTime()}`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("Skeleton source not found");
+        }
+        const jsonObj = await response.json();
+        return jsonObj;
+    } catch (error) {
+        console.warn("Skeleton source not found, loading empty template instead.");
+        const urlEmptyJsonTemplate = `${skeletonEmptyTemplate}?v=${new Date().getTime()}`;
+        const response = await fetch(urlEmptyJsonTemplate);
+        const jsonObj = await response.json();
+        return jsonObj;
+    }
 }
