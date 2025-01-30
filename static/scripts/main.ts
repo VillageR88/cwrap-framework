5002bc3986ecae6070e2f03458b4d11b0255c242
VillageR88 <villager88pl@gmail.com>
2025-01-30 14:32:05 +0100

    Initial mobile nav
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- static/scripts/main.ts
+++ static/scripts/main.ts
@@ -1,4 +1,14 @@
 const version = document.getElementById("version");
+const mobileNavButton = document.getElementById("mobile-nav-button") as
+	| HTMLButtonElement
+	| undefined;
+const nav = document.getElementById("aside-documents") as
+	| HTMLElement
+	| undefined;
+const documentation = document.getElementById("documentation-container") as
+	| HTMLDivElement
+	| undefined;
+
 window.onload = async () => {
 	if (version) {
 		try {
@@ -16,3 +26,32 @@ window.onload = async () => {
 		}
 	}
 };
+
+if (mobileNavButton) {
+	mobileNavButton.onclick = () => {
+		if (nav && documentation) {
+			nav.style.opacity = nav.style.opacity === "1" ? "0" : "1";
+			nav.style.userSelect = nav.style.userSelect === "auto" ? "none" : "auto";
+			nav.style.pointerEvents =
+				nav.style.pointerEvents === "auto" ? "none" : "auto";
+			if (documentation.style.opacity === "0") {
+				documentation.style.opacity = "1";
+				setTimeout(() => {
+					documentation.style.display = "flex";
+				}, 100);
+			} else {
+				documentation.style.opacity = "0";
+				setTimeout(() => {
+					documentation.style.display = "none";
+				}, 100);
+			}
+			documentation.style.userSelect =
+				documentation.style.userSelect === "none" ? "auto" : "none";
+			documentation.style.pointerEvents =
+				documentation.style.pointerEvents === "none" ? "auto" : "none";
+
+			document.body.style.overflowY =
+				document.body.style.overflowY === "clip" ? "auto" : "clip";
+		}
+	};
+}
